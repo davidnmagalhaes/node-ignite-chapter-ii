0ec3d14a858142b403489c47d35a6593669e150f
David Magalhães <david_nbxnb@hotmail.com>
2023-04-28 08:58:33 -0300

    Specification Module

---
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -0,0 +1,12 @@
+interface ICreateSpecificationDTO {
+  name: string;
+  description: string;
+}
+
+interface ISpecificationsRepository {
+  // findByName(name: string): Category;
+  // list(): Category[];
+  create({ name, description }: ICreateSpecificationDTO): void;
+}
+
+export { ISpecificationsRepository, ICreateSpecificationDTO };
